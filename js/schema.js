ab27f56307a4a03a14a2f894bed8349de602b775
vurtal248 <chadwayan@gmail.com>
2026-04-02 10:23:00 -0600

    Update schema.js

--- js/schema.js
+++ js/schema.js
@@ -25,7 +25,7 @@ export const EDITABLE_KEYS = new Set(
 export const COMPUTED_COLS = METRICS_SCHEMA.filter(c => c.computed);
 
 export const SEED_DATA = [
-  { id: 3, mpg: 36.5, apg: 8.2, rpg: 5.4, spg: 1.2, bpg: 0.8, topg: 2.1, fgm: 11, fga: 22, tpm: 4, tpa: 9, ftm: 6.5, fta: 7 },
-  { id: 2, mpg: 34.0, apg: 9.0, rpg: 6.0, spg: 2.0, bpg: 1.0, topg: 1.8, fgm: 9, fga: 18, tpm: 3, tpa: 8, ftm: 7, fta: 8 },
-  { id: 1, mpg: 30.0, apg: 10.5, rpg: 4.2, spg: 0.8, bpg: 0.2, topg: 3.5, fgm: 8, fga: 15, tpm: 2, tpa: 6, ftm: 4, fta: 5 }
+  { id: 3, mpg: 36, apg: 8, rpg: 5, spg: 1, bpg: 0, topg: 2, fgm: 11, fga: 22, tpm: 4, tpa: 9, ftm: 6, fta: 7 },
+  { id: 2, mpg: 34, apg: 9, rpg: 6, spg: 2, bpg: 1, topg: 1, fgm: 9, fga: 18, tpm: 3, tpa: 8, ftm: 7, fta: 8 },
+  { id: 1, mpg: 30, apg: 10, rpg: 4, spg: 0, bpg: 0, topg: 3, fgm: 8, fga: 15, tpm: 2, tpa: 6, ftm: 4, fta: 5 }
 ];
