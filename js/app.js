b9ace46861e4077c092ea33e2406db7617a9cd5e
vurtal248 <chadwayan@gmail.com>
2026-04-06 19:52:28 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -816,7 +816,6 @@ class BMetricsApp {
         targets: updatedNodes,
         color: [tc.textSecondary, tc.accent],
         scale: [0.95, 1],
-        textShadow: ['none', '0 0 16px ' + tc.accent],
         duration: 800,
         delay: anime.stagger(50),
         easing: 'easeOutExpo'
